e3a3c869e103cf88a7ff4d214ad819c12f490428
Mustafa Selçuk Çağlar <selcukcaglar08@gmail.com>
2020-04-30 00:00:32 +0300

    BenthamDataset implementation started

--- Advanced_mxnet/ocr/utils/iam_dataset.py
+++ Advanced_mxnet/ocr/utils/iam_dataset.py
@@ -38,7 +38,7 @@ def resize_image(image, desired_size):
     ''' Helper function to resize an image while keeping the aspect ratio.
     Parameter
     ---------
-    
+
     image: np.array
         The image to be resized.
 
@@ -62,12 +62,12 @@ def resize_image(image, desired_size):
         new_size = tuple([int(x*ratio) for x in size])
         image = cv2.resize(image, (new_size[1], new_size[0]))
         size = image.shape
-            
+
     delta_w = max(0, desired_size[1] - size[1])
     delta_h = max(0, desired_size[0] - size[0])
     top, bottom = delta_h//2, delta_h-(delta_h//2)
     left, right = delta_w//2, delta_w-(delta_w//2)
-            
+
     color = image[0][0]
     if color < 230:
         color = 230
@@ -81,18 +81,18 @@ def crop_handwriting_page(image, bb, image_size):
     '''
     Given an image and bounding box (bb) crop the input image based on the bounding box.
     The final output image was scaled based on the image size.
-    
+
     Parameters
     ----------
     image: np.array
         Input form image
-    
+
     bb: (x, y, w, h)
         The bounding box in percentages to crop
-        
+
     image_size: (h, w)
         Image size to scale the output image to.
-        
+
     Returns
     -------
     output_image: np.array
@@ -116,11 +116,11 @@ class IAMDataset(dataset.ArrayDataset):
         To select the method of parsing the images of the passage
         Available options: [form, form_bb, line, word]
 
-    credentials: (str, str), Default None 
+    credentials: (str, str), Default None
         Your (username, password) for the IAM dataset. Register at
         http://www.fki.inf.unibe.ch/DBs/iamDB/iLogin/index.php
         By default, IAMDataset will read it from credentials.json
-    
+
     root: str, default: dataset/iamdataset
         Location to save the database
 
@@ -130,12 +130,12 @@ class IAMDataset(dataset.ArrayDataset):
     output_data_type: str, default text
         What type of data you want as an output: Text or bounding box.
         Available options are: [text, bb]
-     
+
     output_parse_method: str, default None
-        If the bounding box (bb) was selected as an output_data_type, 
+        If the bounding box (bb) was selected as an output_data_type,
         this parameter can select which bb you want to obtain.
         Available options: [form, line, word]
-        
+
     output_form_text_as_array: bool, default False
         When output_data is set to text and the parse method is set to form or form_original,
         if output_form_text_as_array is true, the output text will be a list of lines string
@@ -144,7 +144,7 @@ class IAMDataset(dataset.ArrayDataset):
     MAX_IMAGE_SIZE_LINE = (60, 800)
     MAX_IMAGE_SIZE_WORD = (30, 140)
     def __init__(self, parse_method, credentials=None,
-                 root=os.path.join(os.path.dirname(__file__), '..', '..','dataset', 'iamdataset'), 
+                 root=os.path.join(os.path.dirname(__file__), '..', '..','dataset', 'iamdataset'),
                  train=True, output_data="text",
                  output_parse_method=None,
                  output_form_text_as_array=False):
@@ -176,7 +176,7 @@ class IAMDataset(dataset.ArrayDataset):
                 assert False, "Please enter credentials for the IAM dataset in credentials.json or as arguments"
         else:
             self._credentials = credentials
-        
+
         self._train = train
 
         _output_data_types = ["text", "bb"]
@@ -202,7 +202,7 @@ class IAMDataset(dataset.ArrayDataset):
         if not os.path.isdir(root):
             os.makedirs(root)
         self._output_form_text_as_array = output_form_text_as_array
-        
+
         data = self._get_data()
         super(IAMDataset, self).__init__(data)
 
@@ -257,7 +257,7 @@ class IAMDataset(dataset.ArrayDataset):
         urllib.request.install_opener(opener)
         opener.open(url)
         filename = os.path.basename(url)
-        print("Downloading {}: ".format(filename)) 
+        print("Downloading {}: ".format(filename))
         urllib.request.urlretrieve(url, reporthook=self._reporthook,
                                    filename=os.path.join(self._root, filename))[0]
         sys.stdout.write("\n")
@@ -270,7 +270,7 @@ class IAMDataset(dataset.ArrayDataset):
         if not os.path.isfile(archive_file):
             self._download(self._xml_url)
             self._extract(archive_file, archive_type="tar", output_dir="xml")
-            
+
     def _download_data(self):
         ''' Helper function to download and extract the data of the IAM database
         '''
@@ -290,7 +290,7 @@ class IAMDataset(dataset.ArrayDataset):
             logging.info("Downloding subject list from {}".format(url))
             self._download(url)
             self._extract(archive_file, archive_type="zip", output_dir="subject")
-    
+
     def _pre_process_image(self, img_in):
         im = cv2.imread(img_in, cv2.IMREAD_GRAYSCALE)
         if np.size(im) == 1: # skip if the image data is corrupt.
@@ -303,13 +303,13 @@ class IAMDataset(dataset.ArrayDataset):
         if self._parse_method == "word":
             im, _ = resize_image(im, self.MAX_IMAGE_SIZE_WORD)
         img_arr = np.asarray(im)
-        return img_arr 
+        return img_arr
 
     def _get_bb_of_item(self, item, height, width):
         ''' Helper function to find the bounding box (bb) of an item in the xml file.
         All the characters within the item are found and the left-most (min) and right-most (max + length)
-        are found. 
-        The bounding box emcompasses the left and right most characters in the x and y direction. 
+        are found.
+        The bounding box emcompasses the left and right most characters in the x and y direction.
 
         Parameter
         ---------
@@ -341,14 +341,14 @@ class IAMDataset(dataset.ArrayDataset):
         y2 = float(y2) / height
         bb = [x1, y1, x2 - x1, y2 - y1]
         return bb
-    
+
     def _get_output_data(self, item, height, width):
         ''' Function to obtain the output data (both text and bounding boxes).
         Note that the bounding boxes are rescaled based on the rescale_ratio parameter.
 
         Parameter
         ---------
-        item: xml.etree 
+        item: xml.etree
             XML object for a word/line/form.
 
         height: int
@@ -390,7 +390,7 @@ class IAMDataset(dataset.ArrayDataset):
             Bounding boxes (x, y, w, h) in percentages to be converted.
 
         relative_bb: [int, int, int, int]
-            Reference bounding box (in percentages) to convert bb to 
+            Reference bounding box (in percentages) to convert bb to
 
         bb_reference_size: (int, int)
             Size (h, w) in pixels of the image containing bb
@@ -408,8 +408,8 @@ class IAMDataset(dataset.ArrayDataset):
         -------
         bb: [[int, int, int, int]]
             Bounding boxes (x, y, w, h) in percentages that are converted
-        
-        '''        
+
+        '''
         (x1, y1, x2, y2) = (bb[:, 0], bb[:, 1], bb[:, 0] + bb[:, 2], bb[:, 1] + bb[:, 3])
         (x1, y1, x2, y2) = (x1 * bb_reference_size[1], y1 * bb_reference_size[0],
                             x2 * bb_reference_size[1], y2 * bb_reference_size[0])
@@ -439,7 +439,7 @@ class IAMDataset(dataset.ArrayDataset):
         expand_bb_scale = 0.05
         new_w = (1 + expand_bb_scale) * bb[2]
         new_h = (1 + expand_bb_scale) * bb[3]
-        
+
         bb[0] = bb[0] - (new_w - bb[2])/2
         bb[1] = bb[1] - (new_h - bb[3])/2
         bb[2] = new_w
@@ -447,7 +447,7 @@ class IAMDataset(dataset.ArrayDataset):
 
         image_arr_bb = crop_image(image_arr, bb)
 
-        if self._output_data == "bb":            
+        if self._output_data == "bb":
             output_data = self._change_bb_reference(output_data, bb, image_arr.shape, image_arr.shape, image_arr_bb.shape, "minus")
 
         image_arr_bb_, bb = resize_image(image_arr_bb, desired_size=(700, 700))
@@ -456,12 +456,12 @@ class IAMDataset(dataset.ArrayDataset):
             output_data = self._change_bb_reference(output_data, bb, image_arr_bb.shape, image_arr_bb_.shape, image_arr_bb_.shape, "plus")
         image_arr = image_arr_bb_
         return image_arr, output_data
-    
+
     def _save_dataframe_chunks(self, df, name):
         for i, df_split in enumerate(np.array_split(df, 4)):
             filename = name[:-5] + str(i) + ".plk" # remove *.plk in the filename
             df_split.to_pickle(filename, protocol=2)
-            
+
     def _load_dataframe_chunks(self, name):
         image_data_chunks = []
         for fn in sorted(glob.glob(name)):
@@ -473,7 +473,7 @@ class IAMDataset(dataset.ArrayDataset):
     def _process_data(self):
         ''' Function that iterates through the downloaded xml file to gather the input images and the
         corresponding output.
-        
+
         Returns
         -------
         pd.DataFrame
@@ -513,10 +513,10 @@ class IAMDataset(dataset.ArrayDataset):
         ''' Function to organise the list of subjects to training and testing.
         The IAM dataset provides 4 files: trainset, validationset1, validationset2, and testset each
         with a list of subjects.
-        
+
         Parameters
         ----------
-        
+
         train_subject_lists: [str], default ["trainset", "validationset1", "validationset2"]
             The filenames of the list of subjects to be used for training the model
 
@@ -560,10 +560,10 @@ class IAMDataset(dataset.ArrayDataset):
 
     def _convert_subject_list(self, subject_list):
         ''' Function to convert the list of subjects for the "word" parse method
-        
+
         Parameters
         ----------
-        
+
         subject_lists: [str]
             A list of subjects
 
@@ -583,10 +583,10 @@ class IAMDataset(dataset.ArrayDataset):
             return new_subject_list
         else:
             return subject_list
-                
+
     def _get_data(self):
         ''' Function to get the data and to extract the data for training or testing
-        
+
         Returns
         -------
 
